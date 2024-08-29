a693c4343133996f0e8c80cc12c647141b1cf3b6
Marcel Moura <bicmoura@gmail.com>
2024-08-29 09:27:39 -0300

    Filter logic implemented, but not keepig filter states.

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,22 +6,27 @@ import 'package:comida/models/category.dart';
 import 'package:comida/models/meal.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key, required this.onToggleFavorite});
+  const CategoriesScreen({
+    super.key,
+    required this.onToggleFavorite,
+    required this.availableMeals,
+  });
 
-  final void Function (Meal meal) onToggleFavorite;
+  final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
 
   //in Stateless widgets Context is not globally available
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = dummyMeals
+    final filteredMeals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
-            title: category.title,
-            meals: filteredMeals,
-            onToggleFavorite: onToggleFavorite,
+          title: category.title,
+          meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
     ); //ALT: Navigator.push(context, route);
@@ -30,21 +35,21 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          children: [
-            //availableCategories.map((e) => CategoryGridItem(category: category)).toList()
-            for (final category in availableCategories)
-              CategoryGridItem(
-                category: category,
-                onSelectCategory: () {
-                  _selectCategory(context, category);
-                },
-              )
-          ],
-        );
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children: [
+        //availableCategories.map((e) => CategoryGridItem(category: category)).toList()
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          )
+      ],
+    );
   }
 }
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,9 +1,17 @@
+import 'package:comida/data/dummy_data.dart';
 import 'package:comida/screens/categories.dart';
 import 'package:comida/screens/filters.dart';
 import 'package:comida/screens/meals.dart';
 import 'package:comida/models/meal.dart';
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
+
+const kInitialFilters = {
+  Filter.glutenFree: false,
+  Filter.lactoseFree: false,
+  Filter.vegetarian: false,
+  Filter.vegan: false,
+};
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -17,13 +25,12 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
+  late Map<Filter, bool> _selectedFilters = kInitialFilters;
 
-  void _showInfoMessage(String message){
+  void _showInfoMessage(String message) {
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-            content: Text(message)
-        ),
+      SnackBar(content: Text(message)),
     );
   }
 
@@ -48,16 +55,39 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
-  void _setScreen (String identifier){
+  void _setScreen(String identifier) async {
     Navigator.of(context).pop();
-    if(identifier == 'filters'){
-      Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => const FiltersScreen()));
+    if (identifier == 'filters') {
+      final result = await Navigator.of(context).push<Map<Filter, bool>>(
+          MaterialPageRoute(builder: (ctx) => const FiltersScreen()));
+      setState(() {
+        _selectedFilters = result ?? kInitialFilters;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget activePage = CategoriesScreen(onToggleFavorite: _toggleMealFavoriteStatus,);
+    final availableMeals = dummyMeals.where((meal){
+      if(_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree){
+        return false;
+      }
+      if(_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree){
+        return false;
+      }
+      if(_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian){
+        return false;
+      }
+      if(_selectedFilters[Filter.vegan]! && !meal.isVegan){
+        return false;
+      }
+      return true;
+    }).toList();
+
+    Widget activePage = CategoriesScreen(
+      onToggleFavorite: _toggleMealFavoriteStatus,
+      availableMeals: availableMeals,
+    );
     var activePageTitle = 'Categories';
     if (_selectedPageIndex == 1) {
       activePage = MealsScreen(
